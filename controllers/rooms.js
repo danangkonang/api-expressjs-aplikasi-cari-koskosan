4e94399a752868f1b6b730a2a74d023bb46d1af3
Danang <danang@localhost.localdomain>
2019-08-29 14:02:44 +0700

    repare error

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -141,10 +141,6 @@ exports.delete=(req, res)=>{
         .catch(err => {
           res.send(err.message)
         })
-<<<<<<< HEAD
-    // res.send({tes:req.params.id})
-=======
->>>>>>> 5263124473b4dbe9188d5d95d730ba91bcf8a60c
 }
 
 exports.input = (req, res) => {
